35293611080dfacbbd7d26be2d3d18dc93252de5
GregOSF <gregonza1@MacBook-Bro.local>
2015-07-06 18:02:37 -0700

    Added code and templating

--- w3_d1_2_apis_continued/spotify_app/script.js
+++ w3_d1_2_apis_continued/spotify_app/script.js
@@ -1,5 +1,93 @@
 $(function() {
 
   // your code here
+  var $searchForm = $('#spotify-search');
+  var $track = $('#track');
+  var $resultList = $('#results');
+  
+
+
+  $searchForm.on('submit', function() {
+  	event.preventDefault();
+  	$.get (
+  	'https://api.spotify.com/v1/search?type=track&q=' + $track.val(),
+  	function (data) {
+  		var songName = data.tracks.items[0].artists[0].name;
+  		var album = data.tracks.items[0].album.name;
+  		var artist = data.tracks.items[0].name;
+  		
+
+  		console.log(songName);
+  		console.log(artist);	
+  		console.log(album);
+  		
+
+  		var songTemplate = _.template($('#songTemp').html())
+  		var newSong = new NewTrax(songName, artist, album); 
+
+  		
+  		function NewTrax(songName, artist, album) {
+  			this.songName = songName;
+  			this.artist = artist;
+  			this.album = album;
+  		}
+
+  		NewTrax.all = [];
+
+  		NewTrax.prototype.save = function () {
+  			NewTrax.all.push(this);
+  		}
+
+  		NewTrax.prototype.render= function () {
+  			var $newTrack = $(songTemplate(this));
+  			this.index = NewTrax.all.indexOf(this);
+  			$newTrack.attr('data-index', this.index);
+  			$resultList.prepend($newTrack);
+  		}
+
+
+  		newSong.save();
+  		newSong.render();
+
+  		
+
+  		
+  		})});
+
+/*
+  		console.log(data.tracks.items[0].artists[0].name);
+  		console.log(data.tracks.items[0].album.name);
+  		console.log(data.tracks.items[0].name);
+
+
+  	/*	console.log(data.tracks.items)*/
+  	
+
+  /*var songTemplate = _.template($('#songTemp').html)
+  var newSong = new NewTrax(); 
+
+  function NewTrax(songName, artist, album) {
+  	this.songName = songName;
+  	this.artist = artist;
+  	this.album = album;
+  }
+
+  NewTrax.all = [];
+
+  NewTrax.prototype.save = function () {
+  	NewTrax.all.push.(this);
+  }
+
+  NewTrax.prototype.render= function () {
+  	var $newTrack = $(songTemplate(this));
+  	this.index = NewTrax.all.indexOf(this);
+  	$newTrack.attr('data-index', this.index);
+  	$resultList.prepend($newTrack);
+  }
+*/
+
+
+
+
 
 });
